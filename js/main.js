355f11b96cf5d8c9e90ca5ec6ef52502f07ce6da
pablohk <pablusmmid@gmail.com>
2018-01-15 22:01:59 +0100

    make transparent background images; change nav links; change details to contact; starter proyects section

--- js/main.js
+++ js/main.js
@@ -9,29 +9,52 @@ const logoPath = [
   "./media/img/mongodb-logo.png",
   "./media/img/node-logo.png",
 ];
-
 $(document).ready(function() {
   loadLogos(logoPath);
-  //let idlogoMix=setInterval(logoShuffle, 1000*2 );
-  //let Idmix=setInterval(mix, 1000*2 );
+
+// Update the logos img each 2 seconds
+  //let Idmix = setInterval(mix, 1000 * 2);
+  showDetails();
   // END document ready
 });
 
+// Shuffle the logos show in header tag
 function logoShuffle() {
   loadLogos(_.shuffle(logoPath));
 }
 
-function mix(){
-  let rdm = (()=>Math.floor(Math.random()*logoPath.length));
-  let initial = rdm(), final = rdm();
-  let initialEl = $('header .logo:nth-child('+(initial +1)+')');
-  let finalEl=$('header .logo:nth-child('+(final +1)+')');
+// Show / hide  the previous DOM elements of clicked element
+function showDetails() {
+  $('.exp-show').click(function(e) {
+    e.preventDefault();
+    let el = $(this).text();
+    el = el === "Show details......" ? "Hide......" : "Show details......";
+    $(this).html(el);
+    $(this).prev().toggle();
+  });
+}
+
+// Show  the Contact DOM elements
+function showContact() {
+  $('#contact').css('visibility','visible');
+}
+
+//  Change random 2 elements in array and show all of them on the front
+function mix() {
+  //let rdm = (()=>Math.floor(Math.random()*logoPath.length));
+  let rdm = function() {
+    return Math.floor(Math.random() * logoPath.length);
+  };
+  let initial = rdm(),final = rdm();
+  let initialEl = $('header .logo:nth-child(' + (initial + 1) + ')');
+  let finalEl = $('header .logo:nth-child(' + (final + 1) + ')');
   initialEl.attr('src', logoPath[final]);
   finalEl.attr('src', logoPath[initial]);
 }
 
-function loadLogos(arr){
-  $("header .logo").each(function(index){
-    $(this).attr('src',arr[index]);
+// Load logos on the front
+function loadLogos(arr) {
+  $("header .logo").each(function(index) {
+    $(this).attr('src', arr[index]);
   });
 }
